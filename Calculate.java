8595e2d5e0b19343c194f25f17f79f058282cd7c
Shevchuk Pavel <pshevchuk@ukrpost.net>
2016-03-20 12:22:58 +0200

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -5,6 +5,7 @@ public class Calculate {
  		 System.out.println("Calculate..."); 
 		int first = Integer.valueOf(arg[0]);
 		int second = Integer.valueOf(arg[1]);
+		int x = 0;
 		int summ = first + second;
  		 System.out.println(" Summ " + first + " and " + second + " = " + summ + " \n"); 
 
